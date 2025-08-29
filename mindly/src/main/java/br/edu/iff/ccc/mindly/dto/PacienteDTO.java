4a4ecc1f5e6bbc9a3062494452d76c46d7c68d8a
Victor Soares <victor9soares@gmail.com>
2025-08-29 16:31:25 -0300

    Feat: Inserção de PacienteDTO

--- mindly/src/main/java/br/edu/iff/ccc/mindly/dto/PacienteDTO.java
+++ mindly/src/main/java/br/edu/iff/ccc/mindly/dto/PacienteDTO.java
@@ -1,5 +1,95 @@
 package br.edu.iff.ccc.mindly.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+
 public class PacienteDTO {
-    
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private String telefone;
+    private String endereco;
+
+    @NotNull
+    private LocalDate dataNascimento;
+
+    @NotBlank
+    @Size(min = 11, max = 14)
+    private String cpf;
+
+    private String planoSaude;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPlanoSaude() {
+        return planoSaude;
+    }
+
+    public void setPlanoSaude(String planoSaude) {
+        this.planoSaude = planoSaude;
+    }
 }
