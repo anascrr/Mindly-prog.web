56d21647923024cf19b396bd29283ca87cc5215b
anascrr <ajuliascorrea@gmail.com>
2025-09-28 23:15:21 -0300

    fix: ajuste em acentuação corrompida no banco de dados

--- mindly/src/main/resources/data.sql
+++ mindly/src/main/resources/data.sql
@@ -11,10 +11,10 @@ INSERT INTO usuarios (id, nome, email, senha, role)
 VALUES (4444444, 'Dra. Helena Vasconcelos', 'psiquiatra@mindly.com', '123', 'PSIQUIATRA');
 
 INSERT INTO pacientes (id, nome, email, telefone, endereco, data_nascimento, cpf, plano_saude)
-VALUES (1111111, 'João da Silva', 'joao@mindly.com', '123456789', 'Rua A, 123', '1990-01-01', '123.456.789-00', 'Unimed');
+VALUES (1111111, 'João da Silva', 'joao@mindly.com', '123456789', 'Rua A, 123', '01/01/1990', '123.456.789-00', 'Unimed');
 
 INSERT INTO pacientes (id, nome, email, telefone, endereco, data_nascimento, cpf, plano_saude)
-VALUES (2222222, 'Maria Oliveira', 'maria@mindly.com', '987654321', 'Rua B, 456', '1985-05-05', '987.654.321-00', 'Bradesco Saúde');
+VALUES (2222222, 'Maria Oliveira', 'maria@mindly.com', '987654321', 'Rua B, 456', '05/05/1985', '987.654.321-00', 'Bradesco Saúde');
 
 INSERT INTO pacientes (id, nome, email, telefone, endereco, data_nascimento, cpf, plano_saude)
-VALUES (3333333, 'Pedro Santos', 'pedro@mindly.com', '456789123', 'Rua C, 789', '2000-10-10', '456.789.123-00', 'Amil');
+VALUES (3333333, 'Pedro Santos', 'pedro@mindly.com', '456789123', 'Rua C, 789', '10/10/2000', '456.789.123-00', 'Amil');
